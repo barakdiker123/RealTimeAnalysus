6093a8f3dc50415c7d757dcf4d7bc1b78a7ea1b3
barak diker <barakdiker@gmail.com>
2022-12-23 14:30:48 +0200

    Added interface for builder Big function
    Added interface for easy integration with patrolling drone
    one should only link against the exported_target_path_planning
    and have the entire interface for his comfort

---
+++ include/exported_target_path_planning.h
@@ -0,0 +1,78 @@
+
+#pragma once
+
+#include "Auxilary.h"
+#include "get_navigation_point.hpp"
+//#include "lemon/core.h"
+#include "visualizer_cloud_and_path.hpp"
+//#include <chrono>
+//#include <fstream>
+//#include <iostream>
+//#include <lemon/bfs.h>
+//#include <lemon/list_graph.h>
+//#include <lemon/maps.h>
+//#include <list>
+//#include <pcl/common/common_headers.h>
+//#include <pcl/console/parse.h>
+//#include <pcl/features/normal_3d.h>
+//#include <pcl/io/pcd_io.h>
+//#include <pcl/visualization/pcl_visualizer.h>
+#include <random>
+#include <sstream>
+#include <thread>
+
+class PathBuilder {
+private:
+  float ScaleFactor = 0.2;
+  float byHowMuchChangeScaleFactor = 0.1;
+  int howManyTimesTriesEachScaleFactor = 10;
+  int howLongIsAValidPath = 10;
+  bool debug = true;
+
+public:
+  PathBuilder(float ScaleFactor) : ScaleFactor(ScaleFactor) {}
+  PathBuilder() {}
+  /**
+   * @brief This function takes parameters and
+   * return the path to the unknown via path_to_the_unknown
+   * variable
+   * @param cloud[in] -> Entire model
+   * @param StartPoint[in] -> Start Point
+   * @param knownPoint1[in] -> Points needed for creating the plane for RRT
+   * @returns path_to_the_unknown[out] -> The returned path to the unknown
+   * */
+  std::list<pcl::PointXYZ> operator()(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,
+                                      pcl::PointXYZ StartPoint,
+                                      pcl::PointXYZ knownPoint1,
+                                      pcl::PointXYZ knownPoint2,
+                                      pcl::PointXYZ knownPoint3
+
+  );
+  /**
+   * @brief This function takes parameters and
+   * return the path to the unknown via path_to_the_unknown
+   * variable
+   * @param cloud[in] -> Entire model
+   * @param StartPoint[in] -> Start Point
+   * @param knownPoint1[in] -> Points needed for creating the plane for RRT
+   * @param path_to_the_unknown[out] -> The returned path to the unknown
+   * */
+  void operator()(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,
+                  pcl::PointXYZ StartPoint, pcl::PointXYZ knownPoint1,
+                  pcl::PointXYZ knownPoint2, pcl::PointXYZ knownPoint3,
+                  std::list<pcl::PointXYZ> &path_to_the_unknown);
+  /**
+   * @brief This function takes parameters and
+   * return the path to the unknown via path_to_the_unknown
+   * variable
+   * @override create text file
+   * @param cloud[in] -> Entire model
+   * @param StartPoint[in] -> Start Point
+   * @param knownPoint1[in] -> Points needed for creating the plane for RRT
+   * @param path_to_the_unknown[out] -> The returned path to the unknown
+   * */
+  void operator()(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,
+                  pcl::PointXYZ StartPoint, pcl::PointXYZ knownPoint1,
+                  pcl::PointXYZ knownPoint2, pcl::PointXYZ knownPoint3,
+                  std::string location_file_path_to_the_unknown);
+};
